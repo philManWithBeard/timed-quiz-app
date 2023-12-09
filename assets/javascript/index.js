a1c94468a0b8c6a18de51787217c5a158aff0892
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2023-12-09 10:02:24 +0000

    Pseudocode

---
+++ assets/javascript/index.js
@@ -0,0 +1,35 @@
+// Start button
+
+//      Event Listener
+
+//      Start Quiz
+
+//      Start Timer
+
+// Timer
+
+//      Set Count Interval
+
+//      Select DOM Element
+
+// Quiz
+
+//      Remove Start Elements
+
+//      Present question
+
+//      Present answers
+
+//      Present correct or incorrect status
+
+// State
+
+//      Hold state Question
+
+//      Hold state answer status
+
+//      Store state in localstorage
+
+//      Hold username
+
+//      Hold total score
