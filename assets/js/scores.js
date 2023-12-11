f974475c4daf99bb1d844332155a19f2d986b8af
Philip Burrows <48831325+philManWithBeard@users.noreply.github.com>
2023-12-11 13:17:00 +0000

    Clear score

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,22 +1,45 @@
-// Start Button
+/*--- Selectors ---*/
+
+// High Scores
 const highScoresEl = document.querySelector("#highscores");
 
+// Clear Button
+const clearButtonEl = document.querySelector("#clear");
+
+/*--- Display High Scores ---*/
+
+// Set userScore global
 const userScore = JSON.parse(localStorage.getItem("userScore"));
 
-const sortedUserScore = userScore.sort((a, b) => {
-  let x = a.score;
-  let y = b.score;
-  if (y > x) {
-    return 1;
-  }
-  if (y < x) {
-    return -1;
-  }
-  return 0;
-});
+if (userScore !== null) {
+  userScore.sort((a, b) => {
+    let x = a.score;
+    let y = b.score;
+    if (y > x) {
+      return 1;
+    }
+    if (y < x) {
+      return -1;
+    }
+    return 0;
+  });
 
-userScore.forEach((element) => {
-  const userScoreLi = document.createElement("li");
-  highScoresEl.appendChild(userScoreLi);
-  userScoreLi.textContent = element.initials + " - " + element.score;
+  userScore.forEach((element) => {
+    const userScoreLi = document.createElement("li");
+    highScoresEl.appendChild(userScoreLi);
+    userScoreLi.textContent = element.initials + " - " + element.score;
+  });
+}
+
+/*--- Event Listeners ---*/
+
+//  Clear Button
+clearButtonEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+  localStorage.clear("userScore");
+
+  while (highScoresEl.firstChild) {
+    highScoresEl.removeChild(highScoresEl.firstChild);
+  }
 });
